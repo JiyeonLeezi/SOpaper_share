c38610b6ce7a4a94bd7c1788cabe2668c4531735
Jiyeon "Leezie" Lee <jiyeonlee2029@u.northwestern.edu>
2025-07-25 19:42:56 -0500

    Update code.m

---
+++ Scanned/code.m
@@ -0,0 +1,3 @@
+load('whiskerdata.mat', 'data'); load RAT03_RM.mat; % load(['RAT03_',SO,'.mat']);
+PTS_3D = [data.basepoint(1,:); data.RM]; % 6 whisker points (Nx3 matrix)
+PTS_2D = RM; % Fit whisker centerline from scan data
